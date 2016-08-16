421ea25621ed9ce1c955efad112cc922415643d8
William E. Seyler <wseyler@pentaho.com>
2016-08-16 09:16:52 -0400

    [BACKLOG-8410] - Changed tooltips to use the "title" tag (#2814)

--- plugins/repositories-plugin/src/main/resources/web/js/controllers.js
+++ plugins/repositories-plugin/src/main/resources/web/js/controllers.js
@@ -401,6 +401,7 @@ define(
       $scope.help = function() {
         help();
       }
+      $scope.addToolTips = addToolTips;
     });
 
     repoConnectionAppControllers.controller("RepositoryConnectController", function($scope, $rootScope, repositoryConnectModel) {
@@ -428,6 +429,19 @@ define(
           close();
         }
       }
+      $scope.addToolTips = addToolTips;
     });
+    
+    function addToolTips() {
+      var eles = document.querySelectorAll('.mightOverflow');
+      setTimeout(function() {
+        for (var i = 0; i < eles.length; i++) {
+          var ele = eles[i];
+          if (ele.offsetWidth < ele.scrollWidth) {
+            ele.title = ele.innerText;
+          }
+        }
+      });
+    }
   }
 );
