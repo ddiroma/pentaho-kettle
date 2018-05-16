311622f022b9338a676650218c98fa9fdb851e91
Dean DiRoma <ddiroma@pentaho.com>
2018-05-16 10:14:19 -0400

    DBDIALOG skeleton framework

---
+++ plugins/database-connection/core/src/main/javascript/app/app.component.js
@@ -0,0 +1,77 @@
+/*!
+ * HITACHI VANTARA PROPRIETARY AND CONFIDENTIAL
+ *
+ * Copyright 2018 Hitachi Vantara. All rights reserved.
+ *
+ * NOTICE: All information including source code contained herein is, and
+ * remains the sole property of Hitachi Vantara and its licensors. The intellectual
+ * and technical concepts contained herein are proprietary and confidential
+ * to, and are trade secrets of Hitachi Vantara and may be covered by U.S. and foreign
+ * patents, or patents in process, and are protected by trade secret and
+ * copyright laws. The receipt or possession of this source code and/or related
+ * information does not convey or imply any rights to reproduce, disclose or
+ * distribute its contents, or to manufacture, use, or sell anything that it
+ * may describe, in whole or in part. Any reproduction, modification, distribution,
+ * or public display of this information without the express written authorization
+ * from Hitachi Vantara is strictly prohibited and in violation of applicable laws and
+ * international treaties. Access to the source code contained herein is strictly
+ * prohibited to anyone except those individuals and entities who have executed
+ * confidentiality and non-disclosure agreements or other agreements with Hitachi Vantara,
+ * explicitly covering such access.
+ */
+
+/**
+ * The Database Connection Main App component.
+ *
+ * This provides the main component for supporting the database connection functionality.
+ * @module app.component
+ * @property {String} name The name of the Angular component.
+ * @property {Object} options The JSON object containing the configurations for this component.
+ **/
+define([
+  "text!./app.html",
+  "pentaho/i18n-osgi!database-connection.messages",
+  "angular",
+  "css!./app.css"
+], function(template, i18n, angular) {
+  "use strict";
+
+  var options = {
+    bindings: {},
+    template: template,
+    controllerAs: "vm",
+    controller: appController
+  };
+
+  appController.$inject = ["$location", "$scope", "$timeout", "$state"];
+
+  /**
+   * The App Controller.
+   *
+   * This provides the controller for the app component.
+   *
+   * @param {Object} dt - Angular service that contains helper functions for the app component controller
+   * @param {Function} $location - Angular service used for parsing the URL in browser address bar
+   * @param {Object} $scope - Application model
+   * @param {Object} $timeout - Angular wrapper around window.setTimeout
+   */
+  function appController($location, $scope, $timeout, $state) {
+    var vm = this;
+    vm.$onInit = onInit;
+
+
+    /**
+     * The $onInit hook of components lifecycle which is called on each controller
+     * after all the controllers on an element have been constructed and had their
+     * bindings initialized. We use this hook to put initialization code for our controller.
+     */
+    function onInit() {
+
+    }
+  }
+
+  return {
+    name: "databaseConnectionApp",
+    options: options
+  };
+});
